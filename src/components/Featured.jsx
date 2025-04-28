574b0be46438423e31918abc1c59589c01e459b8
williamgao24 <williamgao24@gmail.com>
2025-04-28 13:53:29 -0400

    created genres component

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,28 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import './Featured.css'
+import './Featured.css';
 
 function Featured() {
     const [movies, setMovies] = useState([]);
-  
+
     useEffect(() => {
-      async function getData() {
-        setMovies((await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`)).data.results);
-      };
-  
-      getData();
+        async function getData() {
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`
+            );
+            const shuffledMovies = response.data.results.sort(() => 0.5 - Math.random()); // Shuffle the movies
+            setMovies(shuffledMovies.slice(0, 4));
+        }
+
+        getData();
     }, []);
-  
+
     return (
-      <div className="movie-container">
-        {movies && movies.map(movie => (
-          <div className="movie-card" key={movie.id}>
-            <h1>{`${movie.title}`}</h1>
-            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.id}`} />
-          </div>
-        ))}
-      </div>
+        <div className="movie-container">
+            {movies.slice(0,4).map(movie => (
+                <div className="movie-card" key={movie.id}>
+                    <h1>{movie.title}</h1>
+                    <img
+                        className="movie-poster"
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                    />
+                </div>
+            ))}
+        </div>
     );
-  }
-  
-  export default Featured;
+}
+
+export default Featured;
